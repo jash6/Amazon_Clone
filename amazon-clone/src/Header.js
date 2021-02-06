3ea44f7193642edfa013aa0af401e25f2a46c26f
jashmakhija <jashmakhija06@gmail.com>
2021-02-06 13:27:13 +0530

    first commit

---
+++ amazon-clone/src/Header.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+function Header() {
+  return (
+    <nav className="header">
+      {/* logo on  the left */}
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+        />
+      </Link>
+      {/* Search Box */}
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      {/* links */}
+      <div className="header__nav">
+        {/* 1st link */}
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello</span>
+            <span className="header__optionLineTwo">Sign In</span>
+          </div>
+        </Link>
+        {/* 2nd link */}
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+        {/* 3rd link */}
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
+          </div>
+        </Link>
+        {/* Basket icon with number */}
+        <Link to="/checkout" className="header__link">
+          <div className="header__optionBasket">
+            {/* Shopping basket icon*/}
+            <ShoppingBasketIcon />
+            {/* Shopping basket number*/}
+            <span className="header__optionLineTwo header__BasketCount">0</span>
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
